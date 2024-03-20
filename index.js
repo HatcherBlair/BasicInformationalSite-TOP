18b7f648831cd1edcf7630bf9edfaa37db166173
Hatcher Blair <hatcherbb@gmail.com>
2024-03-20 14:24:47 -0600

    Converted to express

--- index.js
+++ index.js
@@ -1,42 +1,25 @@
-const http = require("http");
-const fs = require("fs");
+const express = require("express");
 const path = require("path");
+const app = express();
 
-const hostname = "127.0.0.1";
 const port = 8080;
 
-const server = http.createServer((req, res) => {
-  let page = null;
-  switch (req.url) {
-    case "/":
-      page = "index.html";
-      break;
-
-    case "/about":
-      page = "about.html";
-      break;
-
-    case "/contact-me":
-      page = "contact-me.html";
-      break;
-
-    default:
-      page = "404.html";
-  }
-
-  const filePath = path.join(__dirname, page);
-  fs.readFile(filePath, (err, data) => {
-    if (err) {
-      res.statusCode = 500;
-      res.end(`Error loading ${page}`);
-    } else {
-      res.statusCode = 200;
-      res.setHeader("Content-Type", "text/html");
-      res.end(data);
-    }
-  });
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}`);
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/about.html"));
+});
+
+app.get("/contact-me", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/contact-me.html"));
+});
+
+app.get("*", (req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "./public/404.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
